09190767748a317ec7cf1e8f0d7221be95a3bb88
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-08-21 06:50:17 +0000

    Enhance Doctypes with new fields.

--- frappe_whatsapp/frappe_whatsapp/doctype/occasion_invitee/occasion_invitee.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/occasion_invitee/occasion_invitee.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Shridhar Patil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 from frappe_whatsapp.qr_code import get_qr_code
@@ -9,4 +9,13 @@ from frappe_whatsapp.qr_code import get_qr_code
 
 class OccasionInvitee(Document):
     def validate(self):
+        self.check_duplicate()
         self.qr_raw_data = get_qr_code(self.name)
+    
+    def check_duplicate(self):
+        exists = frappe.db.exists(
+        "Occasion Invitee",
+        {"occasion": self.occasion, "invitee": self.invitee}
+    )
+        if exists and exists != self.name:
+            frappe.throw("This invitee is already linked to the selected occasion.")
