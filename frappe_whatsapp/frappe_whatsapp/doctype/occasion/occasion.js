d8f6891ae44fff7aa1f05d52b81c0ab17035e640
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-08-21 13:18:42 +0000

    Filter template field to show only APPROVED WhatsApp Templates.

--- frappe_whatsapp/frappe_whatsapp/doctype/occasion/occasion.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/occasion/occasion.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, Shridhar Patil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Occasion", {
-// 	refresh(frm) {
+frappe.ui.form.on("Occasion", {
+  refresh(frm) {
+    frm.set_query("invite_template", function () {
+      return {
+        filters: {
+          status: "APPROVED",
+        },
+      };
+    });
 
-// 	},
-// });
+    frm.set_query("confirmed_template", function () {
+      return {
+        filters: {
+          status: "APPROVED",
+        },
+      };
+    });
+
+    frm.set_query("declined_template", function () {
+      return {
+        filters: {
+          status: "APPROVED",
+        },
+      };
+    });
+  },
+});
--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
@@ -1,13 +1,20 @@
 // Copyright (c) 2022, Shridhar Patil and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('WhatsApp Message', {
-	refresh: function(frm) {
-		if (frm.doc.type == 'Incoming'){
-			frm.add_custom_button(__("Reply"), function(){
-				frappe.new_doc("WhatsApp Message", {"to": frm.doc.from});
+frappe.ui.form.on("WhatsApp Message", {
+  refresh: function (frm) {
+    if (frm.doc.type == "Incoming") {
+      frm.add_custom_button(__("Reply"), function () {
+        frappe.new_doc("WhatsApp Message", { to: frm.doc.from });
+      });
+    }
 
-			});
-		}
-	}
+    frm.set_query("template", function () {
+      return {
+        filters: {
+          status: "APPROVED",
+        },
+      };
+    });
+  },
 });
