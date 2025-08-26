d542fc4d0dae089f64115af53010172e06fd907f
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-08-26 18:52:58 +0000

    Add method to clear WhatsApp notification logs older than given days (default 3)

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification_log/whatsapp_notification_log.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification_log/whatsapp_notification_log.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2022, Shridhar Patil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class WhatsAppNotificationLog(Document):
-	pass
+    @staticmethod
+    def clear_old_logs(days=3):
+        from frappe.query_builder import Interval
+        from frappe.query_builder.functions import Now
+
+        table = frappe.qb.DocType("WhatsApp Notification Log")
+        frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
